c71dbe458cd86e8c55a624c7dc6371fa39fde1ee
Tim Rogers <tim@gocardless.com>
2012-10-09 13:37:04 +0100

    Updates frontend to use new stored data

--- app.rb
+++ app.rb
@@ -8,31 +8,6 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 set :port, 8227 # 8227 spells "UCAS" ;)
 
 get '/' do
-  @results = UCAS::Datastore.all()
-  
-  # Here lies skanky codes, since UCAS have no API...
-  @course_codes = {
-    "L0V0" => {
-      "university" => "University of Oxford",
-      "course" => "Politics, Philosophy and Economics"
-    },
-    "L290" => {
-      "university" => "Birkbeck, University of London",
-      "course" => "Politics and Government"
-    },
-    "L202" => {
-      "university" => "SOAS, University of London",
-      "course" => "Politics"
-    },
-    "LV25" => {
-      "university" => "London School of Economics",
-      "course" => "Politics and Philosophy"
-    },
-    "L230" => {
-      "university" => "London School of Economics",
-      "course" => "Government"
-    }
-  }
-  
+  @results = UCAS::Datastore.all()  
   erb :index
 end
