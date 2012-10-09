fa84f3beb600bdcf1f7f38303b25eb5fd33056c7
Tim Rogers <tim@gocardless.com>
2012-10-09 13:33:57 +0100

    Merges in latest UCAS::Datastore with namespacing and hash storage support. Updates special "all" method to support this

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+VERSION = "2.0" # Choose the dataset you want to grab from
+
 # Require in all the files from lib/
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 
--- lib/datastore.rb
+++ lib/datastore.rb
@@ -1,28 +1,32 @@
 require 'redis'
+require 'redis-namespace'
+require 'json'
 
 module UCAS
   class Datastore
     @@redis = Redis.new
+    @@adapter = Redis::Namespace.new("ucas-#{VERSION}".to_sym, :redis => @@redis)
     
     def self.redis
-      @@redis
+      @@adapter
     end
     
     def self.get(key)
-      @@redis.get(key)
+      JSON.parse(@@adapter.get(key), symbolize_names: true)
     end
     
     def self.set(key, value)
-      @@redis.set(key, value)
+      @@adapter.set(key, value.to_json)
     end
     
     def self.all
-      keys = @@redis.keys("L*")
+      keys = @@adapter.keys("*")
       results = {}
       keys.each do |key|
         results[key] = self.get(key)
       end
       results
     end
+    
   end
 end
